5fb398965d43199b3e6bd9b40b6bb74d8388badf
TyreseJin <tyresejin3915@gmail.com>
2023-05-20 14:21:00 +0900

    fix: add email.checker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import { notification } from 'antd';
 import validator from 'email-validator';
+import axios from 'axios';
 import Home from './components/Home';
 import Content from './components/Content';
 import Schedule from './components/Schedule';
+import {ZERO_BOUNCE_API_KEY } from './const/constants';
 
 function App() {
     const [currentPage, setCurrentPage] = useState('Home');
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
+
+    // const checkEmail = async (email) => {
+    //     const url = `https://apilayer.net/api/check?access_key=${MAILBOXLAYER_API_KEY}&email=${email}`;
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     return data.format_valid && data.mx_found;
+    // }
+
+    const checkEmail = async (email) => {
+        const apiKey = 'YOUR_API_KEY';
+        const url = `https://api.zerobounce.net/v2/validate?api_key=${ZERO_BOUNCE_API_KEY}&email=${email}`;
+        const response = await axios.get(url);
+        const data = response.data;
+        return data.status === 'Valid';
+    }
 
     return (
         <div>
@@ -17,7 +34,7 @@ function App() {
                     <Content scheduleAction={() => setCurrentPage('Schedule')} backAction={() => setCurrentPage('Home')} />
                     : currentPage === 'Schedule' ?
                         <Schedule backAction={() => setCurrentPage('Content')} username={username} email={email} />
-                        : <Home goAction={() => {
+                        : <Home goAction={async () => {
                             if (!username) {
                                 notification.warning({
                                     message: 'Please enter your name!'
@@ -31,6 +48,13 @@ function App() {
                                 return;
                             }
                             if (!validator.validate(email)) {
+                                notification.warning({
+                                    message: 'Please enter a email address!'
+                                });
+                                return;
+                            }
+                            const isValid = await checkEmail(email);
+                            if (!isValid) {
                                 notification.warning({
                                     message: 'Please enter a real email address!'
                                 });
