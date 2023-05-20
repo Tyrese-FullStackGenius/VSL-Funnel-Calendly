e625fbe753b92fc40b9f241d3138f31da7dbb4d8
TyreseJin <tyresejin3915@gmail.com>
2023-05-20 15:45:44 +0900

    fix: change some error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { notification } from 'antd';
 import validator from 'email-validator';
-import axios from 'axios';
+// import axios from 'axios';
 import Home from './components/Home';
 import Content from './components/Content';
-import Schedule from './components/Schedule';
-import {ZERO_BOUNCE_API_KEY } from './const/constants';
+// import {ZERO_BOUNCE_API_KEY, MAILBOXLAYER_API_KEY } from './const/constants';
 
 function App() {
     const [currentPage, setCurrentPage] = useState('Home');
@@ -20,48 +19,47 @@ function App() {
     // }
 
     const checkEmail = async (email) => {
-        const url = `https://api.zerobounce.net/v2/validate?api_key=${ZERO_BOUNCE_API_KEY}&email=${email}`;
-        const response = await axios.get(url);
-        const data = response.data;
-        console.log(data);
-        return data.status === 'Valid';
+        // const url = `https://api.zerobounce.net/v2/validate?api_key=${ZERO_BOUNCE_API_KEY}&email=${email}`;
+        // const response = await axios.get(url);
+        // const data = response.data;
+        // console.log(data);
+        // return data.status === 'Valid';
+        return true;
     }
 
     return (
         <div>
             {
                 currentPage === 'Content' ?
-                    <Content scheduleAction={() => setCurrentPage('Schedule')} backAction={() => setCurrentPage('Home')} />
-                    : currentPage === 'Schedule' ?
-                        <Schedule backAction={() => setCurrentPage('Content')} username={username} email={email} />
-                        : <Home goAction={async () => {
-                            if (!username) {
-                                notification.warning({
-                                    message: 'Please enter your name!'
-                                });
-                                return;
-                            }
-                            if (!email) {
-                                notification.warning({
-                                    message: 'Please enter your email!'
-                                });
-                                return;
-                            }
-                            if (!validator.validate(email)) {
-                                notification.warning({
-                                    message: 'Please enter a email address!'
-                                });
-                                return;
-                            }
-                            const isValid = await checkEmail(email);
-                            if (!isValid) {
-                                notification.warning({
-                                    message: 'Please enter a real email address!'
-                                });
-                                return;
-                            }
-                            setCurrentPage('Content');
-                        }} setUsername={setUsername} setEmail={setEmail} username={username} email={email} />
+                    <Content scheduleAction={() => setCurrentPage('Schedule')} backAction={() => setCurrentPage('Home')} username={username} email={email} />
+                    : <Home goAction={async () => {
+                        if (!username) {
+                            notification.warning({
+                                message: 'Please enter your name!'
+                            });
+                            return;
+                        }
+                        if (!email) {
+                            notification.warning({
+                                message: 'Please enter your email!'
+                            });
+                            return;
+                        }
+                        if (!validator.validate(email)) {
+                            notification.warning({
+                                message: 'Please enter a email address!'
+                            });
+                            return;
+                        }
+                        const isValid = await checkEmail(email);
+                        if (!isValid) {
+                            notification.warning({
+                                message: 'Please enter a real email address!'
+                            });
+                            return;
+                        }
+                        setCurrentPage('Content');
+                    }} setUsername={setUsername} setEmail={setEmail} username={username} email={email} />
             }
         </div>
     )
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { Row, Col, Button } from 'antd';
+import React, { useState } from 'react';
+import { Row, Col, Button, Typography } from 'antd';
 import { Player } from 'video-react';
+import { PopupModal, useCalendlyEventListener } from 'react-calendly';
+import { CALENDLY_LINK } from '../const/constants';
 import LOGO from '../assets/logo.png';
 import 'video-react/dist/video-react.css';
 
 function Content(props) {
+    const [successFlag, setSuccessFlag] = useState(false);
+    const [isModalOpen, setModalOpen] = useState(false);
+
+    useCalendlyEventListener({
+        onProfilePageViewed: () => console.log("onProfilePageViewed"),
+        onDateAndTimeSelected: () => console.log("onDateAndTimeSelected"),
+        onEventTypeViewed: () => console.log("onEventTypeViewed"),
+        onEventScheduled: (e) => {
+            console.log(e.data.payload);
+            setSuccessFlag(true);
+        },
+    });
+
     return (
         <Row style={{ height: '100vh', width: '100vw', background: '#F2F3F7', overflowY: 'auto' }}>
             <Col md={{ span: 16, offset: 4 }} sm={{ span: 20, offset: 2 }} xs={{ span: 22, offset: 1 }}>
                 <div style={{ textAlign: 'center' }}>
                     <img alt='' src={LOGO} style={{ margin: '12px 0' }} />
                 </div>
+
+                {
+                    successFlag ? <Typography.Title level={3}>Thanks you!</Typography.Title> : <></>
+                }
 
                 <Player>
                     <source src='/assets/video.mp4' controls autoPlay />
@@ -30,10 +49,17 @@ function Content(props) {
                         <Button
                             type='primary'
                             style={{ background: '#D22DE3', color: '#FFFFFF', margin: '8px 0', padding: '12px', height: '48px', width: '100%' }}
-                            onClick={() => props.scheduleAction()}
+                            onClick={() => setModalOpen(true)}
                         >
                             APPLY NOW
                         </Button>
+
+                        <PopupModal
+                            url={CALENDLY_LINK}
+                            open={isModalOpen}
+                            rootElement={document.getElementById("root")}
+                            onClose={() => setModalOpen(false)}
+                        />
                     </Col>
                 </Row>
             </Col>
